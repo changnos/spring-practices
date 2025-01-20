c49137249670ad32eb60db98a6e82ce20b8678a9
Changjin Lee <changjini32@gmail.com>
2025-01-20 10:57:30 +0900

    thymeleaf #2

--- thymeleaf/src/main/java/config/WebConfig.java
+++ thymeleaf/src/main/java/config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.thymeleaf.spring6.ISpringTemplateEngine;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -19,62 +21,73 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"controller"})
+@ComponentScan({ "controller" })
 public class WebConfig implements WebMvcConfigurer {
-    // Resouce Handler
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:assets/");
-    }
+	// Resouce Handler
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:assets/");
+	}
 
-    // Message Source
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages/message");
-        messageSource.setDefaultEncoding("utf-8");
+	// Message Source
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages/message");
+		messageSource.setDefaultEncoding("utf-8");
 
-        return messageSource;
-    }
+		return messageSource;
+	}
 
-    // Thymeleaf01: Template Resolver
-    @Bean
-    public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	// Thymeleaf01: Template Resolver
+	@Bean
+	public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
 
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setPrefix("classpath:templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding("utf-8");
-        templateResolver.setCacheable(false);
+		templateResolver.setApplicationContext(applicationContext);
+		templateResolver.setPrefix("classpath:templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCharacterEncoding("utf-8");
+		templateResolver.setCacheable(false);
 
-        return templateResolver;
-    }
+		return templateResolver;
+	}
 
-    // Thymeleaf02: Template Engine
-    @Bean
-    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	// Thymeleaf02: Template Engine
+	@Bean
+	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 
-        templateEngine.setTemplateResolver(templateResolver);
-        templateEngine.setEnableSpringELCompiler(true);
-        templateEngine.setTemplateEngineMessageSource(messageSource());
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.setEnableSpringELCompiler(true);
+		templateEngine.setTemplateEngineMessageSource(messageSource());
 
-        return templateEngine;
-    }
+		return templateEngine;
+	}
 
-    // Thymeleaf03: View Resolver
-    @Bean
-    public ViewResolver thymeleafViewResolver(ISpringTemplateEngine templateEngine) {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	// Thymeleaf03: View Resolver
+	@Bean
+	public ViewResolver thymeleafViewResolver(ISpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 
-        viewResolver.setTemplateEngine(templateEngine);
-        viewResolver.setCharacterEncoding("UTF-8");
-        viewResolver.setOrder(1);
+		viewResolver.setTemplateEngine(templateEngine);
+		viewResolver.setCharacterEncoding("UTF-8");
+		viewResolver.setOrder(1);
 
-        return viewResolver;
-    }
+		return viewResolver;
+	}
+
+	// View Resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setViewNames("views/*");
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setOrder(0);
+
+		return viewResolver;
+	}
 }
